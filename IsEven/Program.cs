a98999e1d7c0def101f139874a5fae3b9182006b
Krzysztof Orzeł <krzysztof.orzel92@gmail.com>
2023-01-09 21:48:29 +0100

    Add all project files.

---
+++ IsEven/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsEven
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Witaj użytkowniku, podaj liczbę całkowitą, a ja sprawdzę czy jest ona parzysta czy nie parzysta");
+            Console.WriteLine("Podaj liczbę: ");
+
+            var number = GetNumber();
+
+            if (number % 2 == 0)
+                {
+                    Console.WriteLine("Podana liczba jest parzysta");
+                }
+                else Console.WriteLine("Podana liczba jest nieparzysta");
+
+                Console.ReadKey();
+         
+ 
+        }
+
+        private static int GetNumber()
+        {
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out int number))
+                {
+                    Console.WriteLine("Podałeś nieprawidłową wartość, wprowadź liczbę: ");
+                    continue;
+                }
+
+                return number;
+            }
+
+        }
+
+
+    }
+}
